31c4bd3ad5aa8ca338e8d8bd6db1f16c2242f2cc
ojasnaik <naikojas@gmail.com>
2024-03-27 13:14:59 -0700

    Add Cognito authentication to enable upload

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-AWS.config.region = 'us-east-2'; // Region
+AWS.config.region = "us-east-2"; // Region
 
 function uploadFile(file) {
   const s3 = new AWS.S3({
-    apiVersion: '2006-03-01',
-    params: { Bucket: 'ebike-s3' },
+    apiVersion: "2006-03-01",
+    params: { Bucket: "ebike-s3" },
+  });
+
+  // Initialize the Amazon Cognito credentials provider
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "us-east-2:944991fe-ca60-479b-bab3-7d5f2f58799f	",
+  });
+
+  // Get AWS credentials
+  AWS.config.credentials.get(function (err) {
+    if (err) {
+      console.log("Error: " + err);
+      return;
+    }
+    console.log("Cognito Identity Id: " + AWS.config.credentials.identityId);
+
   });
 
   // Use S3 ManagedUpload class to upload the file
   const upload = new AWS.S3.ManagedUpload({
     params: {
-      Bucket: 'ebike-s3',
+      Bucket: "ebike-s3",
       Key: file.name,
       Body: file,
-      ACL: 'public-read', // Or another ACL according to your requirements
+      ACL: "public-read", // Or another ACL according to your requirements
       ContentType: file.type,
-    }
+    },
   });
 
   const promise = upload.promise();
 
   promise.then(
-    function(data) {
+    function (data) {
       console.log("Successfully uploaded photo.");
       alert("Successfully uploaded photo.");
     },
-    function(err) {
+    function (err) {
       console.log("There was an error uploading your photo: ", err.message);
       alert("There was an error uploading your photo: " + err.message);
     }
   );
 }
 
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const files = document.getElementById('imageInput').files;
-  if (files.length > 0) {
-    const file = files[0];
-    uploadFile(file);
-  }
-});
+document
+  .getElementById("uploadForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const files = document.getElementById("imageInput").files;
+    if (files.length > 0) {
+      const file = files[0];
+      uploadFile(file);
+    }
+  });
